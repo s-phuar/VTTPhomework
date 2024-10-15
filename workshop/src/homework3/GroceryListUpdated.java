e4ba5c3975bb9ebac34957b7b44c1ca152aed5c4
s-phuar <coolbeansphuar123@gmail.com>
2024-10-15 17:54:32 +0800

    finished workshop 3

--- workshop/src/homework3/GroceryListUpdated.java
+++ workshop/src/homework3/GroceryListUpdated.java
@@ -2,106 +2,153 @@ package homework3;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+        //javac -d shoppingcart.jar --source-path src src/homework3/GroceryListUpdated.java
+        //java -cp shoppingcart.jar homework3.GroceryListUpdated src/homework3/cartdb
+        // "src/homework3/cartdb" as the args[0]
 
 public class GroceryListUpdated {
+    static String username;    //tracks current user 
 
     public static void main(String[] args) throws IOException, FileNotFoundException{
-        String cartDirectory;   
-        //******************************use "src/homework3/directoryname" as the args[0]
-
+        String cartDirectory;
         if(args.length > 0 ){
             cartDirectory = args[0];
         }else{
             cartDirectory = "db";
         }
 
-        //file to hold name of directory
+        //directory creation 'cartdb'
         File directory = new File(cartDirectory);
-
         if(!directory.exists()){
-            //creates directory as well as return a boolean
-            boolean isDirCreated = directory.mkdir();
+            boolean isDirCreated = directory.mkdir(); //creates directory as well as return a boolean
             if(isDirCreated){
                 System.out.println("Directory created: " + cartDirectory);
             }else{
                 System.out.println("Failed to create directory: " + cartDirectory);
             }
-
         }else{
             System.out.println("Using directory: " + cartDirectory);
         }
 
+        //create blank userList.txt file, stores all usernames
+        Path pUserList = Paths.get(cartDirectory + "/userList.txt");
+        File fileUserList = pUserList.toFile();
+        if(!fileUserList.exists()){
+            fileUserList.createNewFile(); 
+        }
 
-
-
-        //arraylist to hold all ingredients
-        ArrayList<String> groceryList = new ArrayList<>();
-        //HashSet to hold  unique user list
-        HashSet<String> userList = new HashSet<>();
+        ArrayList<String> groceryList = new ArrayList<>();  //Arraylist to hold all ingredients
 
         while(true){
-        //cons to hold console command
-        Console cons = System.console();
-        String cmd = cons.readLine("> ").trim().toLowerCase();
+            Console cons = System.console();
+            String cmd = cons.readLine("> ").trim().toLowerCase();
 
-        String[] cmdSplit;        
+            String[] cmdSplit;        
 
-        if(cmd.contains(",")){
-            cmdSplit = cmd.split(" ");
-            for(int i = 0; i < cmdSplit.length; i++){
-                cmdSplit[i] = cmdSplit[i].replace(",","");
-                }
-            } else{
+            if(cmd.contains(",")){ //when adding ingredients, account for commas
+                cmdSplit = cmd.split(" ");
+                for(int i = 0; i < cmdSplit.length; i++){
+                    cmdSplit[i] = cmdSplit[i].replace(",","");
+                    }
+            }else{
             cmdSplit = cmd.split(" ");
             }
-
-
 
         switch(cmdSplit[0]){
             //new commands
 
             case "login":
-                String line;
-                String user = cmdSplit[1];
-                //store path of new user list
-                File file = new File("src/homework3/" + cartDirectory + "/" + user + ".txt");
+                String user = cmdSplit[1]; //user's name
+                List<String> lines = Files.readAllLines(pUserList); //read userfile.txt into a list
+                //update member field
+                username = user;
 
-                if(userList.contains(user)){
-                    //user exists, list exists
-                    BufferedReader bufferedRead = new BufferedReader(new FileReader(file));
-                    if((line = bufferedRead.readLine()) != null){ 
-                        System.out.printf("%s, your cart contains the following items", user);
-                        for(int i = 0; i < groceryList.size(); i++){
-                            System.out.printf("%d. %s\n", i+1, groceryList.get(i));
-                        }
-                    }else{
-                        System.out.printf("%s, your cart is empty", user);
+                try{
+                if(lines.contains(user)){   //if list contains the user's name
+                    //access the user's file and check if null
+                    Path userName = Paths.get(cartDirectory + "/" + user + ".txt");
+                    File fileUserName = userName.toFile();
+                    FileReader reader = new FileReader(fileUserName);
+                    BufferedReader bufferedReader = new BufferedReader(reader);
+                    //if null, print null
+                    String line;
+                    if ((line = bufferedReader.readLine()) != null){    
+                        System.out.printf("%s, your cart contains the following items\n", user);
+                        int counter = 1;
+                        for(String grocery : Files.readAllLines(userName)){
+                            System.out.printf("%d. %s\n", counter, grocery);
+                            counter ++;
+                            }
+                    }else{                                  
+                        System.out.printf("%s, your cart is empty\n", user);
                     }
-                }else{
-                    //user does not exist, list does not exist
-                    //create user, add to user list, and create user's list
-                     //will get created in 'user' directory (homework)
-                    userList.add(user);
-                    file.createNewFile();
 
+                }else{
+                    //write to userList.txt to append new user
+                    System.out.println("user does not exist, creating new user...");
+                    Writer writer = new FileWriter(fileUserList, true);
+                    BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                    bufferedWriter.write(user + "\n");
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    //create blank txt file off new user's name
+                    Path userName = Paths.get(cartDirectory + "/" + user + ".txt");
+                    File fileUserName = userName.toFile();
+                    fileUserName.createNewFile(); 
+                }
+                }catch (IOException e){
+                    System.err.println("Error creating user file: " + e.getMessage());
+                }
+            break;
+
+            case "save":
+                //grab whats in grocerylist, throws it into current user's list
+                if (username != null){
+                Path saveList = Paths.get(cartDirectory + "/" + username + ".txt");
+                File save = saveList.toFile();
+                Writer writer = new FileWriter(save);
+                BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                for(int i = 0; i < groceryList.size(); i++){
+                    bufferedWriter.write(groceryList.get(i));
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                System.out.println("your cart has been saved");
+                }else{
+                    System.out.println("Please login to a user first");
                 }
             break;
 
             case "users":
+                FileReader reader = new FileReader(fileUserList);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                //if null, print null
+                String line;
+                if ((line = bufferedReader.readLine()) != null){    
+                    System.out.println("The following users are registered");
+                    int counter = 1;
+                    for(String name : Files.readAllLines(pUserList)){
+                        System.out.printf("%d. %s\n", counter, name);
+                        counter ++;
+                        }
+                }
             break;
-
-            case "save":
-            break;
-
 
             case "list":
                 if (groceryList.isEmpty()){
